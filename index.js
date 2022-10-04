f2f00ca983f4e30a29d847b8dfca214cbda3fabb
Rickyc81 <riccardo.cosenza81@gmail.com>
2022-10-04 12:52:12 +0200

    Fix: trim sentence

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getAwesomeJobTitle() {
   Object.keys(jobTitles).forEach((i) => {
     myNewPosition += ` ${jobTitles[i][Math.floor(Math.random() * i.length)]} `;
   });
-  return myNewPosition;
+  return myNewPosition.trim();
 }
 
 module.exports = getAwesomeJobTitle;
